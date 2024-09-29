ee3efa1cb6c16c538e1ee39d3199fce604143f80
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-09-30 01:05:46 +0200

    add copy of js code with updates using try.cathc and await

---
+++ incopy.js
@@ -0,0 +1,52 @@
+try {
+    const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    const data = await res.json()
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
+    document.getElementById("author").textContent = `By: ${data.user.name}`
+} catch (err) {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1448375240586-882707db888b?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3Mjc2NDA2NzR8&ixlib=rb-4.0.3&q=85)`
+    document.getElementById("author").textContent =  `By: ${data.user.name}`
+
+try {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    if (!res.ok) {
+        throw Error("Something went wrong")
+    }
+    const data = await res.json()
+    document.getElementById("crypto-top").innerHTML = `
+        <img src=${data.image.small} />
+        <span>${data.name}</span>
+    `
+    document.getElementById("crypto").innerHTML += `
+    /**    <p>🎯: $${data.market_data.current_price.usd}</p>
+    /**    <p>👆: $${data.market_data.high_24h.usd}</p>
+    /**    <p>👇: $${data.market_data.low_24h.usd}</p>
+    `
+} catch (err) {
+    console.error(err)
+}
+
+function getCurrentTime() {
+    const date = new Date()
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-sa", { timeStyle: "short" })
+}
+
+setInterval(getCurrentTime, 1000)
+
+navigator.geolocation.getCurrentPosition(async position => {
+    try {
+        const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+        if (!res.ok) {
+            throw Error("Weather data not available")
+        }
+        const data = await res.json()
+        const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        document.getElementById("weather").innerHTML = `
+            <img src=${iconUrl} />
+            <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+            <p class="weather-city">${data.name}</p>
+        `
+    } catch (err) {
+        console.error(err)
+    }
+});
